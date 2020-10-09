4b5590595b4c2756584e40c38c09e52779f1420d
pirosveta <svetiklanka.2000@yandex.ru>
2020-10-09 22:35:28 +0300

    Scripted auto-commit on change (2020-10-09 22:35:27) by gitwatch.sh

---
+++ src/main/java/ru/bmstu/hadoop/MapSideJoin.java
@@ -0,0 +1,17 @@
+public class MapSideJoin {
+    public int main(String[] args) {
+        JobConf conf = new JobConf(JoinJob.class);
+        conf.setJobName("map join");
+        conf.setInputFormat(CompositeInputFormat.class);
+        FileOutputFormat.setOutputPath(conf, new Path(args[2]));
+        conf.set("mapred.join.expr", CompositeInputFormat.compose("inner",
+                KeyValueTextInputFormat.class,
+                args[0],
+                args[1]
+        ));
+        conf.setMapperClass(MapJoinMapper.class);
+        conf.setOutputKeyClass(Text.class);
+        conf.setOutputValueClass(Text.class);
+        JobClient.runJob(conf);
+    }
+}
