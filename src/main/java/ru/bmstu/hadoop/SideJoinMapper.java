ee8ca69b80cb0006164c85a30094aba802d8e13d
pirosveta <svetiklanka.2000@yandex.ru>
2020-10-09 22:36:23 +0300

    Scripted auto-commit on change (2020-10-09 22:36:23) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/SideJoinMapper.java
+++ src/main/java/ru/bmstu/hadoop/SideJoinMapper.java
@@ -1,3 +1,11 @@
-public class SideJoinMapper {
-    
+public class MapJoinMapper extends MapReduceBase implements
+        Mapper<Text, TupleWritable, Text, Text> {
+    @Override
+    public void map(Text key, TupleWritable value,
+                    OutputCollector<Text, Text> output,
+                    Reporter reporter) throws IOException {
+        Text call = (Text) value.get(0);
+        Text system = (Text) value.get(1);
+        output.collect(call, system);
+    }
 }
