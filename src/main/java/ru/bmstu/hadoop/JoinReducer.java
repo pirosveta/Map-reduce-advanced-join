06a6f7e4ac7398f277ee71a418c66bf2c3412560
pirosveta <svetiklanka.2000@yandex.ru>
2020-10-12 16:53:29 +0300

    Scripted auto-commit on change (2020-10-12 16:53:29) by gitwatch.sh

---
+++ src/main/java/ru/bmstu/hadoop/JoinReducer.java
@@ -0,0 +1,13 @@
+public class JoinReducer extends Reducer<TextPair, Text, Text, Text> {
+    @Override
+    protected void reduce(TextPair key, Iterable<Text> values, Context context) throws
+            IOException, InterruptedException {
+        Iterator<Text> iter = values.iterator();
+        Text systemInfo = new Text(iter.next());
+        while (iter.hasNext()) {
+            Text call = iter.next();
+            Text outValue = new Text(call.toString() + "\t" + systemInfo.toString());
+            context.write(key.getFirst(), outValue);
+        }
+    }
+}
