afd2e34732c829403f05e166a24b238e51fd17c9
pirosveta <svetiklanka.2000@yandex.ru>
2020-10-12 17:11:20 +0300

    Scripted auto-commit on change (2020-10-12 17:11:20) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/HashPartitioner.java
+++ src/main/java/ru/bmstu/hadoop/HashPartitioner.java
@@ -1,3 +1,5 @@
+package ru.bmstu.hadoop;
+
 public class HashPartitioner<K, V> extends Partitioner<K, V> {
     public int getPartition(K key, V value, int numReduceTasks) {
         return (key.hashCode() & Integer.MAX_VALUE) % numReduceTasks;
