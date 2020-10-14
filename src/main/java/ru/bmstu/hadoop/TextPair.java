edb05b02f0bdd6f1bed706a8662d460e6dd944c3
pirosveta <svetiklanka.2000@yandex.ru>
2020-10-14 11:16:48 +0300

    Scripted auto-commit on change (2020-10-14 11:16:48) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/TextPair.java
+++ src/main/java/ru/bmstu/hadoop/TextPair.java
@@ -3,8 +3,13 @@ package ru.bmstu.hadoop;
 import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.Partitioner;
 
-public class TextPair{
-    public static class FirstPartitioner extends Partitioner {
+public class TextPair extends org.apache.hadoop.mapreduce.Partitioner {
+    @Override
+    public int getPartition(Object o, Object o2, int numPartitions) {
+        return 0;
+    }
+
+    public static class FirstPartitioner implements Partitioner {
 
         @Override
         public int getPartition(Object key, Object value, int numPartitions) {
