f6e2ec23022de8c7a8433a1f30386b9dbcd9f5fa
pirosveta <svetiklanka.2000@yandex.ru>
2020-10-15 15:49:29 +0300

    Scripted auto-commit on change (2020-10-15 15:49:29) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/SystemsJoinMapper.java
+++ src/main/java/ru/bmstu/hadoop/SystemsJoinMapper.java
@@ -10,7 +10,8 @@ import java.io.IOException;
 public class SystemsJoinMapper extends Mapper<LongWritable, Text, TextPair, Text> {
     @Override
     protected void map(LongWritable key, Text value, WrappedReducer.Context context) throws IOException, InterruptedException {
-        if ()
+        if ();
+        
         SystemInfo system = new SystemInfo(value);
         context.write(new TextPair(system.getSystemCode().toString(),"0"), new Text(system.toString()));
     }
