175086b97a1c119b4cec4f791ad85afa83253cf1
pirosveta <svetiklanka.2000@yandex.ru>
2020-10-16 23:09:04 +0300

    Scripted auto-commit on change (2020-10-16 23:09:04) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/SystemsJoinMapper.java
+++ src/main/java/ru/bmstu/hadoop/SystemsJoinMapper.java
@@ -7,6 +7,8 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class SystemsJoinMapper extends Mapper<LongWritable, Text, TextPair, Text> {
+    private static final REGEX = "\"", REPLACE = "";
+    
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         if (key.get() > 0) {
