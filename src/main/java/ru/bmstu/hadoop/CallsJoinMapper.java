b185efa5dc2e31950450bd03539248849883371f
pirosveta <svetiklanka.2000@yandex.ru>
2020-10-16 22:10:45 +0300

    Scripted auto-commit on change (2020-10-16 22:10:45) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/CallsJoinMapper.java
+++ src/main/java/ru/bmstu/hadoop/CallsJoinMapper.java
@@ -16,7 +16,6 @@ public class CallsJoinMapper extends Mapper<LongWritable, Text, TextPair, Text> 
                     String destAirportId = columns[14], delay = columns[17];
                     context.write(new TextPair(destAirportId, "1"), new Text(delay));
                 }
-                System.out.println("DA");
             }
             catch (NumberFormatException e) {
             }
