b2a760da3a5ff3c8226ad0235f467674e7739526
pirosveta <svetiklanka.2000@yandex.ru>
2020-10-17 20:44:56 +0300

    Scripted auto-commit on change (2020-10-17 20:44:56) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/FlightJoinMapper.java
+++ src/main/java/ru/bmstu/hadoop/FlightJoinMapper.java
@@ -7,16 +7,17 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class FlightJoinMapper extends Mapper<LongWritable, Text, TextPair, Text> {
+    private static final String COMMA = ",", SECOND_KEY = "1";
     private static final int COLUMN_ID = 14, COLUMN_DELAY = 17;
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         if (key.get() > 0) {
-            String[] columns = value.toString().split(",");
+            String[] columns = value.toString().split(COMMA);
             try {
                 if (Double.parseDouble(columns[COLUMN_DELAY]) > 0) {
                     String destAirportId = columns[COLUMN_ID], delay = columns[COLUMN_DELAY];
-                    context.write(new TextPair(destAirportId, "1"), new Text(delay));
+                    context.write(new TextPair(destAirportId, SECOND_KEY), new Text(delay));
                 }
             }
             catch (NumberFormatException e) {
