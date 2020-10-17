7aa5cf2b3fa6400832146f4b23c74a71ef4c7205
pirosveta <svetiklanka.2000@yandex.ru>
2020-10-17 08:59:44 +0300

    Scripted auto-commit on change (2020-10-17 08:59:44) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/SystemsJoinMapper.java
+++ src/main/java/ru/bmstu/hadoop/SystemsJoinMapper.java
@@ -8,12 +8,13 @@ import java.io.IOException;
 
 public class SystemsJoinMapper extends Mapper<LongWritable, Text, TextPair, Text> {
     private static final String REGEX = "\"", REPLACE = "";
+    private static final int COLUMN_ID = 0, COLUMN_NAME = 1;
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         if (key.get() > 0) {
             String[] columns = value.toString().split(",", 2);
-            String destAirportId = columns[0].replaceAll(REGEX, REPLACE), nameAirport = columns[1];
+            String destAirportId = columns[COLUMN_ID].replaceAll(REGEX, REPLACE), nameAirport = columns[COLUMN_NAME];
             context.write(new TextPair(destAirportId, "0"), new Text(nameAirport));
         }
     }
