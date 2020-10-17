02ef5738977e4aee419441c9bf5daa5f0ea6dfcb
pirosveta <svetiklanka.2000@yandex.ru>
2020-10-17 09:00:36 +0300

    Scripted auto-commit on change (2020-10-17 09:00:36) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/CallsJoinMapper.java
+++ src/main/java/ru/bmstu/hadoop/CallsJoinMapper.java
@@ -7,13 +7,15 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class CallsJoinMapper extends Mapper<LongWritable, Text, TextPair, Text> {
+    private static final int COLUMN_ID = 14, COLUMN_DELAY = 17;
+
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         if (key.get() > 0) {
             String[] columns = value.toString().split(",");
             try {
-                if (Double.parseDouble(columns[17]) > 0) {
-                    String destAirportId = columns[14], delay = columns[17];
+                if (Double.parseDouble(columns[COLUMN_DELAY]) > 0) {
+                    String destAirportId = columns[COLUMN_ID], delay = columns[COLUMN_DELAY];
                     context.write(new TextPair(destAirportId, "1"), new Text(delay));
                 }
             }
